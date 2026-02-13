e5884337477f116b08f29e22875ea79e91d94ba2
bulfan <tudorbulf@gmail.com>
2026-02-13 17:56:46 +0100

    New method for the substitution cipher passed all tests

--- Assignment_1/Substitution_Cipher.py
+++ Assignment_1/Substitution_Cipher.py
@@ -1,59 +1,38 @@
 import sys
 
 
-def encrypt(text, key):
-    if (not key.isalpha() and int(key) % 26 == 0) or key == "abcdefghijklmnopqrstuvwxyz":
-        return text
-    if not key.isalpha():
-        key = int(key) % 26
-        for i in range(len(text)):
-            if text[i].isalpha():
-                if text[i].isupper():
-                    text[i] = chr((ord(text[i]) - ord('A') + key) % 26 + ord('A'))
-                else:
-                    text[i] = chr((ord(text[i]) - ord('a') + key) % 26 + ord('a'))
-    else:
-        key = list(key)
-        for i in range(len(text)):
-            if text[i].isalpha():
-                if text[i].isupper():
-                    text[i] = key[ord(text[i]) - ord('A')].upper()
-                else:
-                    text[i] = key[ord(text[i]) - ord('a')].lower()
-    return text
-
-
-def decrypt(text, key):
-    if (not key.isalpha() and int(key) % 26 == 0) or key == "abcdefghijklmnopqrstuvwxyz":
-        return text
-    if not key.isalpha():
-        key = int(key) % 26
-        for i in range(len(text)):
-            if text[i].isalpha():
-                if text[i].isupper():
-                    text[i] = chr((ord(text[i]) - ord('A') - key) % 26 + ord('A'))
-                else:
-                    text[i] = chr((ord(text[i]) - ord('a') - key) % 26 + ord('a'))
-    else:
-        key = list(key)
-        for i in range(len(text)):
-            if text[i].isalpha():
-                if text[i].isupper():
-                    text[i] = chr(key.index(text[i].lower()) + ord('A'))
-                else:
-                    text[i] = chr(key.index(text[i]) + ord('a'))
-    return text
-
-
 if __name__ == "__main__":
     requests = input().split()
     text = sys.stdin.read()
     text = list(text)
+    transformations = "abcdefghijklmnopqrstuvwxyz"
+    transformations = list(transformations)
     for i in range(0, len(requests), 2):
         request = requests[i]
         key = requests[i + 1]
-        if request == "e":
-            text = encrypt(text, key)
-        elif request == "d":
-            text = decrypt(text, key)
+        if (not key.isalpha() and int(key) % 26 == 0) or key == "abcdefghijklmnopqrstuvwxyz":
+            continue
+        if not key.isalpha():
+            key = int(key) % 26
+            if request == "e":
+                for j in range(26):
+                    transformations[j] = chr((ord(transformations[j]) - ord('a') + key) % 26 + ord('a'))
+            elif request == "d":
+                for j in range(26):
+                    transformations[j] = chr((ord(transformations[j]) - ord('a') - key) % 26 + ord('a'))
+        else:
+            key = list(key)
+            if request == "e":
+                for j in range(26):
+                    transformations[j] = key[ord(transformations[j]) - ord('a')]
+            elif request == "d":
+                key = list(key)
+                for j in range(26):
+                    transformations[j] = chr(key.index(transformations[j]) + ord('a'))
+    for i in range(len(text)):
+        if text[i].isalpha():
+            if text[i].isupper():
+                text[i] = transformations[ord(text[i]) - ord('A')].upper()
+            else:
+                text[i] = transformations[ord(text[i]) - ord('a')]
     print(''.join(text), end="")
